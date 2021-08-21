fc7e627a1c0eec9ca807cc05cb7c72c7d0669522
Darren Jensen <darren.jensen@gmail.com>
2021-08-21 11:12:59 +0700

    added update object values using Object.entries and map

--- javascript/filter.js
+++ javascript/filter.js
@@ -28,3 +28,24 @@ const result = pendingTransactions.filter((tx) => {
 });
 
 console.log(result)
+
+// example of adjusting all values in an object without knowing the keys upfront:
+
+let gasPrice = 111111
+let maxFeePerGas = 88888
+let maxPriorityFeePerGas = 77777
+
+let gasLegacy = { gasPrice }
+let gasEIP1559 = { maxFeePerGas, maxPriorityFeePerGas }
+
+function adjustGasPrice(gas) {
+	updatedGas = {}
+	Object.entries(gas).map(([key, val]) => updatedGas[key] = val * 2)
+	return updatedGas
+}
+
+let newGasLegacy = adjustGasPrice(gasLegacy)
+let newGasEIP1559 = adjustGasPrice(gasEIP1559)
+
+console.log('newGasLegacy', newGasLegacy)
+console.log('newGasEIP1559', newGasEIP1559)
